18c76e422dc354f7630396f958e7765ca7e574b1
Breno Alves <brenower@outlook.com>
2025-07-25 13:32:41 -0300

    feat: add methods for managing PersistentDataContainer in Globals class

--- app/src/main/java/dev/metlhedd/paperts/Globals.java
+++ app/src/main/java/dev/metlhedd/paperts/Globals.java
@@ -2,15 +2,19 @@ package dev.metlhedd.paperts;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,9 +22,10 @@ class EventHandler {
   private Function<Event, Void> handler;
   private Listener listener;
 
-  public EventHandler(Function<Event, Void> handler, Listener listener) {
+  public EventHandler(Function<Event, Void> handler) {
     this.handler = handler;
-    this.listener = listener;
+    this.listener = new Listener() {
+    };
   }
 
   public Listener getListener() {
@@ -70,7 +75,8 @@ class CommandExecutor extends BukkitCommand {
 
 /**
  * Globals class for managing global event handlers and commands.
- * This class allows for registering and unregistering event handlers and commands.
+ * This class allows for registering and unregistering event handlers and
+ * commands.
  */
 public class Globals {
   /**
@@ -99,6 +105,7 @@ public class Globals {
    * 
    * @param plugin The JavaPlugin instance for the plugin.
    */
+
   public Globals(JavaPlugin plugin) {
     this.plugin = plugin;
 
@@ -111,7 +118,7 @@ public class Globals {
    *
    * @param eventClass The class of the event to register.
    * @param handler    The function to handle the event.
-   * @param <T>       The type of the event.
+   * @param <T>        The type of the event.
    */
   public <T extends Event> void registerEvent(Class<T> eventClass, Function<Event, Void> handler) {
     // Check if the event class is already registered
@@ -119,8 +126,7 @@ public class Globals {
       throw new RuntimeException("Event " + eventClass.getName() + " is already registered.");
     }
 
-    EventHandler eventHandler = new EventHandler(handler, new Listener() {
-    });
+    EventHandler eventHandler = new EventHandler(handler);
 
     Bukkit.getPluginManager().registerEvent(eventClass, eventHandler.getListener(),
         org.bukkit.event.EventPriority.NORMAL,
@@ -159,12 +165,12 @@ public class Globals {
    * Registers a command with the plugin's command map.
    * If the command is already registered, it will throw an exception.
    *
-   * @param commandName    The name of the command.
-   * @param description    The description of the command.
-   * @param usageMessage   The usage message for the command.
-   * @param permission     The permission required to execute the command.
-   * @param aliases        The aliases for the command.
-   * @param handler        The function to handle the command execution.
+   * @param commandName  The name of the command.
+   * @param description  The description of the command.
+   * @param usageMessage The usage message for the command.
+   * @param permission   The permission required to execute the command.
+   * @param aliases      The aliases for the command.
+   * @param handler      The function to handle the command execution.
    * @throws RuntimeException if the command is already registered.
    */
   public void registerCommand(String commandName,
@@ -206,5 +212,106 @@ public class Globals {
       this.plugin.getServer().getCommandMap().getKnownCommands().remove(commandName);
     }
     this.commands.clear();
+  }
+
+  /**
+   * Retrieves a string value from a PersistentDataContainer.
+   * This method allows for retrieving a string value associated with a specific
+   * key in the container.
+   * 
+   * @param key       The key to look up in the container.
+   * @param container The PersistentDataContainer to retrieve the value from.
+   * @throws IllegalArgumentException if the container is null.
+   * @return The string value associated with the key, or null if not found.
+   */
+  public String getPersistentContainerString(String key, PersistentDataContainer container)
+      throws IllegalArgumentException {
+    if (container == null) {
+      throw new IllegalArgumentException("PersistentDataContainer cannot be null.");
+    }
+
+    return container.get(new NamespacedKey(this.plugin, key), PersistentDataType.STRING);
+  }
+
+  /**
+   * Retrieves a list of strings from a PersistentDataContainer.
+   * This method allows for retrieving a list of strings associated with a
+   * specific
+   * key in the container.
+   * 
+   * @param key       The key to look up in the container.
+   * @param container The PersistentDataContainer to retrieve the list from.
+   * @return An array of strings associated with the key, or an empty array if not
+   *         found.
+   * @throws IllegalArgumentException if the container is null.
+   */
+  public String[] getPersistentContainerStringList(String key, PersistentDataContainer container)
+      throws IllegalArgumentException {
+    if (container == null) {
+      throw new IllegalArgumentException("PersistentDataContainer cannot be null.");
+    }
+
+    List<String> list = container.get(new NamespacedKey(this.plugin, key), PersistentDataType.LIST.strings());
+    String[] array = new String[list.size()];
+
+    return list.toArray(array);
+  }
+
+  /**
+   * Checks if a key exists in a PersistentDataContainer.
+   * This method allows for checking if a specific key is present in the
+   * container.
+   * 
+   * @param key       The key to check for in the container.
+   * @param container The PersistentDataContainer to check.
+   * @return true if the key exists, false otherwise.
+   * @throws IllegalArgumentException if the container is null.
+   */
+  public boolean hasKeyPersistentContainer(String key, PersistentDataContainer container)
+      throws IllegalArgumentException {
+    if (container == null) {
+      throw new IllegalArgumentException("PersistentDataContainer cannot be null.");
+    }
+
+    return container.has(new NamespacedKey(this.plugin, key));
+  }
+
+  /**
+   * Sets a string value in a PersistentDataContainer.
+   * This method allows for setting a string value associated with a specific key
+   * in the container.
+   *
+   * @param key       The key to associate with the value.
+   * @param value     The string value to set.
+   * @param container The PersistentDataContainer to set the value in.
+   * @throws IllegalArgumentException if the container is null.
+   */
+  public void setPersistentContainerString(String key, String value, PersistentDataContainer container)
+      throws IllegalArgumentException {
+    if (container == null) {
+      throw new IllegalArgumentException("PersistentDataContainer cannot be null.");
+    }
+
+    container.set(new NamespacedKey(this.plugin, key), PersistentDataType.STRING, value);
+  }
+
+  /**
+   * Sets a list of strings in a PersistentDataContainer.
+   * This method allows for setting a list of strings associated with a specific
+   * key
+   * in the container.
+   * 
+   * @param key       The key to associate with the list of strings.
+   * @param value     The list of strings to set.
+   * @param container The PersistentDataContainer to set the list in.
+   * @throws IllegalArgumentException if the container is null.
+   */
+  public void setPersistentContainerStringList(String key, String[] value,
+      PersistentDataContainer container) throws IllegalArgumentException {
+    if (container == null) {
+      throw new IllegalArgumentException("PersistentDataContainer cannot be null.");
+    }
+
+    container.set(new NamespacedKey(this.plugin, key), PersistentDataType.LIST.strings(), List.of(value));
   }
 }
